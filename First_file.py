b6a53e51351655caf8092c30eb7ef0e743314f37
alejandro <anikolaev.ieu2023@student.ie.edu>
2025-01-14 18:31:27 +0100

    1st commit

--- First_file.py
+++ First_file.py
@@ -2,3 +2,6 @@ print("this is another example")
 print("i can do this all day")
 print(10+10*7)
 print("this is another example")
+
+
+
---
+++ arithmetic_operators.py
@@ -0,0 +1,7 @@
+print(2+3) # addition
+print(2-3) # subtraction
+print(2*3) # multiplication
+print(type(4/2), 4/2) # division
+print(type(4//2), 4//2) # integer division
+print(9%5) # modulo (reminder of the division/resto)
+print(2**3) # power
